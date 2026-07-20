00e3f5980e9657adc5dc81bd87c92398a88d0f58
Smetank0105 <206859408+Smetank0105@users.noreply.github.com>
2026-07-20 20:25:54 +0700

    Drop cookies DONE

--- 06-CookiesAndSession/cookie.php
+++ 06-CookiesAndSession/cookie.php
@@ -7,7 +7,8 @@
 </head>
 <body>
 	<h1>Cookie</h1>
-	<?php
+	<?php
+	//use Swoole\Http\Request;
 	define('HOUR', 3600);
 	$visitor = false;
 	if(isset($_COOKIE['return']))
@@ -18,5 +19,20 @@
 	}
 	echo $visitor ? 'Welcome back' : 'Hello';
 	?>
+
+<button onclick='ResetCookies()'>Сбросить</button>
+
+<script>
+	function ResetCookies() {
+		let request = new XMLHttpRequest();
+		request.onreadystatechange = function () {
+			if (this.readyState == 4 && this.status == 200) {
+				alert("Куки сброшены");
+			}
+		}
+		request.open('GET', 'reset_cookies.php', true);
+		request.send();
+	}
+</script>
 </body>
 </html>
---
+++ 06-CookiesAndSession/reset_cookies.php
@@ -0,0 +1,3 @@
+<?php
+setcookie("return", "0", time() - 3600);
+?>
