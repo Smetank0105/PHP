9f8f1d5991cbdeca7f399a097fb49ed9f4b4e924
Smetank0105 <206859408+Smetank0105@users.noreply.github.com>
2026-07-22 20:16:02 +0700

    TODO: Teacher

---
+++ 07-OOP/inc/inheritance.php
@@ -0,0 +1,139 @@
+<?php
+class Human
+{
+	public $last_name = "last name" {
+		get {
+			return $this->last_name;
+		}
+		set($value) {
+			$this->last_name = $value;
+		}
+	}
+	public $first_name = "first name" {
+		get {
+			return $this->first_name;
+		}
+		set($value) {
+			$this->first_name = $value;
+		}
+	}
+	public $age = 0 {
+		get {
+			return $this->age;
+		}
+		set($value) {
+			$this->age = $value;
+		}
+	}
+
+	//Constructor:
+	function __construct($last_name, $first_name, $age)
+	{
+		$this->last_name = $last_name;
+		$this->first_name = $first_name;
+		$this->age = $age;
+		echo "<br>HConstructor<br>";
+	}
+	function __destruct()
+	{
+		echo "<br>HDestructor<br>";
+	}
+
+	//Methods:
+	function __tostring()
+	{
+		echo "<pre>";
+		print_r(__CLASS__ . '<br>');
+		//var_dump($this);
+		//print_r($this);
+		echo "</pre>";
+		return "{$this->last_name} {$this->first_name}, {$this->age}";
+	}
+}
+
+class Student extends Human
+{
+	public $speciality = "spec" {
+
+		get {
+			return $this->speciality;
+		}
+		set($value) {
+			$this->speciality = $value;
+		}
+	}
+	public $group = "group" {
+
+		get {
+			return $this->group;
+		}
+		set($value) {
+			$this->group = $value;
+		}
+	}
+	public $rate = 0 {
+
+		get {
+			return $this->rate;
+		}
+		set($value) {
+			$this->rate = $value;
+		}
+	}
+	public $attendance = 0 {
+
+		get {
+			return $this->attendance;
+		}
+		set($value) {
+			$this->attendance = $value;
+		}
+	}
+
+	//Constructor:
+	function __construct($last_name, $first_name, $age, $speciality, $group, $rate, $attendance)
+	{
+		parent::__construct($last_name, $first_name, $age);
+		$this->speciality = $speciality;
+		$this->group = $group;
+		$this->rate = $rate;
+		$this->attendance = $attendance;
+		echo "<br>SConstructor<br>";
+	}
+	function __destruct()
+	{
+		echo "<br>SDestructor<br>";
+	}
+	//Methods:
+	function __tostring()
+	{
+		echo "<pre>";
+		print_r(__CLASS__ . '<br>');
+		//var_dump($this);
+		//print_r($this);
+		echo "</pre>";
+		return parent::__tostring() . ", {$this->speciality}, {$this->group}, {$this->rate}, {$this->attendance}";
+	}
+}
+
+class Graduate extends Student
+{
+	public $subject = "subject" {
+
+		get {
+			return $this->subject;
+		}
+		set($value) {
+			$this->subject = $value;
+		}
+	}
+//Constructor:
+	function __construct($last_name, $first_name, $age, $speciality, $group, $rate, $attendance, $subject)
+	{
+		parent::__construct($last_name, $first_name, $age, $speciality, $group, $rate, $attendance);
+		$this->subject = $subject;
+	}
+}
+
+
+?>
--- 07-OOP/inc/main.php
+++ 07-OOP/inc/main.php
@@ -1,9 +1,30 @@
 <?php
 require_once __DIR__ . '/point.php';
-
+require_once __DIR__ . '/inheritance.php';
+/*
 $A = new Point();
 $A->info();
 $A->x = 7;
 $A->y = 8;
 echo $A;
+*/
+
+$human = new Human("Vercetty", "Tommy", 30);
+echo $human;
+
+$student = new Student("Pinkman", "Jessie", 20, "Chemistry", "WW_220", 90, 95);
+echo $student;
+
+$graduate = new Graduate
+(
+	"Schreder",
+	"Hank",
+	40,
+	"Criminalistic",
+	"OBN",
+	50,
+	70,
+	"How to catch Hiesenberg"
+);
+echo $graduate;
 ?>
